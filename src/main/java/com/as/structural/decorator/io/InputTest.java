74d94221162e4194a1b93cea95d87c3603bb475a
AndriySV <A.S.Elips@gmail.com>
2014-11-26 22:39:54 +0200

    added io

---
+++ src/main/java/com/as/structural/decorator/io/InputTest.java
@@ -0,0 +1,26 @@
+package com.as.structural.decorator.io;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InputTest {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream filterInputStream = new FileInputStream(
+				"src/main/resources/test.txt");
+		BufferedInputStream bufferedInputStream = new BufferedInputStream(
+				filterInputStream);
+		InputStream inputStream = new LowerCaseInputStream(bufferedInputStream);
+
+		int c;
+		while ((c = inputStream.read()) >= 0) {
+			System.out.print((char) c);
+		}
+
+		inputStream.close();
+	}
+
+}
