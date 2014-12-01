338752db42250fd58b9baf638c110afeaf63d7fb
AndriySV <A.S.Elips@gmail.com>
2014-12-01 11:17:17 +0200

    added lvivCherry

---
+++ src/main/java/com/as/creational/abstractFactory/cake/ingredientsFactory/ingredients/LvivCherry.java
@@ -0,0 +1,8 @@
+package com.as.creational.abstractFactory.cake.ingredientsFactory.ingredients;
+
+public class LvivCherry implements Cherry {
+	
+	public LvivCherry() {
+		System.out.println("Cake with the LvivCherry");
+	}
+}
