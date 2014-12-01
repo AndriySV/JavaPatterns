3eb7d0f26b8811f836b02a7e9bb9a9ce0ee83c8b
AndriySV <A.S.Elips@gmail.com>
2014-12-01 11:24:28 +0200

    example cake has been done

--- src/main/java/com/as/creational/abstractFactory/cake/Main.java
+++ src/main/java/com/as/creational/abstractFactory/cake/Main.java
@@ -2,19 +2,29 @@ package com.as.creational.abstractFactory.cake;
 
 import com.as.creational.abstractFactory.cake.factory.CakeFactory;
 import com.as.creational.abstractFactory.cake.factory.KyivFactory;
+import com.as.creational.abstractFactory.cake.factory.LvivFactory;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		CakeFactory kyivFactory = new KyivFactory();
+		CakeFactory lvivFactory = new LvivFactory();
 		
-		Cake cake = kyivFactory.createCacke("apple");
-		cake.bake();
+		Cake kyivCake;
+		Cake lvivCake;
 		
-		cake = kyivFactory.createCacke("kyiv");
-		cake.bake();
+		kyivCake = kyivFactory.createCacke("apple");
+		kyivCake.bake();
 		
+		kyivCake = kyivFactory.createCacke("cherry");
+		kyivCake.bake();
+		
+		lvivCake = lvivFactory.createCacke("apple");
+		lvivCake.bake();
+		
+		lvivCake = lvivFactory.createCacke("cherry");
+		lvivCake.bake();
 		
 	}
 
