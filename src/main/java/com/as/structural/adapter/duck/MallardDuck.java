04bde263bf4aa5f14deb5601cd2d0f3dff6dd55f
AndriySV <A.S.Elips@gmail.com>
2014-12-03 13:54:37 +0200

    Added duck

---
+++ src/main/java/com/as/structural/adapter/duck/MallardDuck.java
@@ -0,0 +1,15 @@
+package com.as.structural.adapter.duck;
+
+public class MallardDuck implements Duck {
+
+	@Override
+	public void quack() {
+		System.out.println("Quack"); 
+	}
+
+	@Override
+	public void fly() {
+		System.out.println("I'm flying");
+	}
+
+}
