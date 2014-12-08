8ccdb18cf3068cfebe0d1f1f3eccbe9a0974da53
AndriySV <A.S.Elips@gmail.com>
2014-12-08 16:15:44 +0200

    Added menuIterator

---
+++ src/main/java/com/as/behavioral/iterator/menuIterator/DinerMenuIterator.java
@@ -0,0 +1,40 @@
+package com.as.behavioral.iterator.menuIterator;
+
+import java.util.Iterator;
+
+public class DinerMenuIterator implements Iterator<MenuItem> {
+	MenuItem[] items;
+	int position = 0;
+
+	public DinerMenuIterator(MenuItem[] items) {
+		this.items = items;
+	}
+
+	public MenuItem next() {
+		MenuItem menuItem = items[position];
+		position = position + 1;
+		return menuItem;
+	}
+
+	public boolean hasNext() {
+		if (position >= items.length || items[position] == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	@Override
+	public void remove() {
+		if (position <= 0) {
+			throw new IllegalStateException
+				("You can't remove an item until you've done at least one next()");
+		}
+		if (items[position-1] != null) {
+			for (int i = position-1; i < (items.length-1); i++) {
+				items[i] = items[i+1];
+			}
+			items[items.length-1] = null;
+		}
+	}
+}
