2122343858f868ddb97beff3938dfc517a63ce40
AndriySV <A.S.Elips@gmail.com>
2014-12-08 16:13:45 +0200

    Uodated menuOwnIterator

--- src/main/java/com/as/behavioral/iterator/menuOwnIterator/MenuTestDrive.java
+++ src/main/java/com/as/behavioral/iterator/menuOwnIterator/MenuTestDrive.java
@@ -1,7 +1,5 @@
 package com.as.behavioral.iterator.menuOwnIterator;
 
-import java.util.*;
-
 public class MenuTestDrive {
 	public static void main(String args[]) {
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
@@ -12,26 +10,4 @@ public class MenuTestDrive {
 		waitress.printMenu();
 	}
 
-/*	public static void printMenu() {
-		PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
-		DinerMenu dinerMenu = new DinerMenu();
-
-		ArrayList breakfastItems = pancakeHouseMenu.getMenuItems();
- 
-		for (int i = 0; i < breakfastItems.size(); i++) {
-			MenuItem menuItem = (MenuItem)breakfastItems.get(i);
-			System.out.print(menuItem.getName());
-			System.out.println("\t\t" + menuItem.getPrice());
-			System.out.println("\t" + menuItem.getDescription());
-		}
-
-		MenuItem[] lunchItems = dinerMenu.getMenuItems();
- 
-		for (int i = 0; i < lunchItems.length; i++) {
-			MenuItem menuItem = lunchItems[i];
-			System.out.print(menuItem.getName());
-			System.out.println("\t\t" + menuItem.getPrice());
-			System.out.println("\t" + menuItem.getDescription());
-		}
-	}*/
 }
