73b4888b6c5157e8a3d8283518bf834fd7923f11
AndriySV <A.S.Elips@gmail.com>
2014-12-17 21:40:44 +0200

    Added the simpleExample

--- src/main/java/com/as/structural/decorator/simpleExample/Main.java
+++ src/main/java/com/as/structural/decorator/simpleExample/Main.java
@@ -1,10 +1,30 @@
 package com.as.structural.decorator.simpleExample;
 
+import com.as.structural.decorator.simpleExample.component.Beverage;
+import com.as.structural.decorator.simpleExample.component.Coffee;
+import com.as.structural.decorator.simpleExample.decorator.Milk;
+import com.as.structural.decorator.simpleExample.decorator.Sugar;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Beverage coffee;
+		
+		coffee = new Coffee();
+		coffee.description();
+		System.out.println();
+		
+		coffee = new Milk(coffee);
+		coffee.description();
+		System.out.println();
+		
+		coffee = new Sugar(coffee);
+		coffee.description();
+		System.out.println();
+		
+		coffee = new Sugar(coffee);
+		coffee.description();
+		System.out.println();
 	}
 
 }
