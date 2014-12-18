e4ecd217fd8972f707c11ff86c74b148b58b8f6c
AndriySV <A.S.Elips@gmail.com>
2014-12-18 16:19:13 +0200

    Added the simpleExample

--- src/main/java/com/as/creational/factoryMethod/simpleExample/Main.java
+++ src/main/java/com/as/creational/factoryMethod/simpleExample/Main.java
@@ -1,9 +1,20 @@
 package com.as.creational.factoryMethod.simpleExample;
 
+import com.as.creational.factoryMethod.simpleExample.factory.Creator;
+import com.as.creational.factoryMethod.simpleExample.factory.EnglishBookCreator;
+import com.as.creational.factoryMethod.simpleExample.product.Book;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		Creator englishBookCreator = new EnglishBookCreator();
+		
+		Book javaEnglish = englishBookCreator.factoryMethod("java");
+		javaEnglish.describe();
+		
+		Book cEnglish = englishBookCreator.factoryMethod("c");
+		cEnglish.describe();
+		
 	}
 
 }
